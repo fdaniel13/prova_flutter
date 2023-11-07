5d916e20cebec0e6bdeac0b40c729dc39ca040b1
Fabio Daniel <fdaniel.aborges@gmail.com>
2023-11-07 19:14:40 -0300

    ajustando layout dos cards da lista

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -78,9 +78,7 @@ class _HomePageState extends State<HomePage> {
                                     value: controller.crud.listText[i]);
                               },
                               separatorBuilder: (_, j) {
-                                return const Divider(
-                                  thickness: 2,
-                                );
+                                return const SizedBox();
                               },
                               itemCount: controller.crud.listText.length);
                         })),
@@ -140,124 +138,139 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _cardText({required TextData value}) {
-    return Container(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.end,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
         children: [
-          const SizedBox(
-            width: 16,
-          ),
-          Expanded(
-            child: Text(
-              value.text!,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.openSans(
-                fontSize: 16,
-                color: Colors.black,
-                fontWeight: FontWeight.w600,
-              ),
+          Container(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Text(
+                    value.text!,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.openSans(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {
+                        textController.text = value.text!;
+                        controller.editTextId = value.id;
+                      },
+                      icon: Icon(
+                        Icons.border_color,
+                      ),
+                    ),
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (_) => AlertDialog(
+                                  title: Text(
+                                    "Deseja mesmo excluir o texto?",
+                                    overflow: TextOverflow.ellipsis,
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.openSans(
+                                      fontSize: 16,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  actions: [
+                                    OutlinedButton(
+                                      onPressed: () async {
+                                        controller.crud
+                                            .removeText(id: value.id!);
+                                        await controller
+                                            .saveText(text: value.text)
+                                            .whenComplete(() => Navigator.of(
+                                                    context,
+                                                    rootNavigator: true)
+                                                .pop());
+                                      },
+                                      style: ButtonStyle(
+                                          padding: MaterialStateProperty.all<
+                                                  EdgeInsets>(
+                                              const EdgeInsets.symmetric(
+                                                  horizontal: 48)),
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  Colors.red),
+                                          shape: MaterialStateProperty.all<
+                                                  RoundedRectangleBorder>(
+                                              RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(24),
+                                          ))),
+                                      child: Text(
+                                        "sim",
+                                        style: GoogleFonts.openSans(
+                                          fontSize: 14,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                    OutlinedButton(
+                                      onPressed: () {
+                                        Navigator.of(context,
+                                                rootNavigator: true)
+                                            .pop();
+                                      },
+                                      style: ButtonStyle(
+                                          padding: MaterialStateProperty.all<
+                                                  EdgeInsets>(
+                                              const EdgeInsets.symmetric(
+                                                  horizontal: 48)),
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  const Color.fromARGB(
+                                                      255, 47, 189, 109)),
+                                          shape: MaterialStateProperty.all<
+                                                  RoundedRectangleBorder>(
+                                              RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(24),
+                                          ))),
+                                      child: Text(
+                                        "não",
+                                        style: GoogleFonts.openSans(
+                                          fontSize: 14,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ));
+                      },
+                      icon: Icon(
+                        Icons.cancel,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IconButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  textController.text = value.text!;
-                  controller.editTextId = value.id;
-                },
-                icon: Icon(
-                  Icons.border_color,
-                ),
-              ),
-              IconButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (_) => AlertDialog(
-                            title: Text(
-                              "Deseja mesmo excluir o texto?",
-                              overflow: TextOverflow.ellipsis,
-                              textAlign: TextAlign.center,
-                              style: GoogleFonts.openSans(
-                                fontSize: 16,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            actions: [
-                              OutlinedButton(
-                                onPressed: () async {
-                                  controller.crud.removeText(id: value.id!);
-                                  await controller
-                                      .saveText(text: value.text)
-                                      .whenComplete(() => Navigator.of(context,
-                                              rootNavigator: true)
-                                          .pop());
-                                },
-                                style: ButtonStyle(
-                                    padding:
-                                        MaterialStateProperty.all<EdgeInsets>(
-                                            const EdgeInsets.symmetric(
-                                                horizontal: 48)),
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.red),
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(24),
-                                    ))),
-                                child: Text(
-                                  "sim",
-                                  style: GoogleFonts.openSans(
-                                    fontSize: 14,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                              OutlinedButton(
-                                onPressed: () {
-                                  Navigator.of(context, rootNavigator: true)
-                                      .pop();
-                                },
-                                style: ButtonStyle(
-                                    padding:
-                                        MaterialStateProperty.all<EdgeInsets>(
-                                            const EdgeInsets.symmetric(
-                                                horizontal: 48)),
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            const Color.fromARGB(
-                                                255, 47, 189, 109)),
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(24),
-                                    ))),
-                                child: Text(
-                                  "não",
-                                  style: GoogleFonts.openSans(
-                                    fontSize: 14,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ));
-                },
-                icon: Icon(
-                  Icons.cancel,
-                  color: Colors.red,
-                ),
-              ),
-            ],
+          const Divider(
+            thickness: 2,
           )
         ],
       ),
